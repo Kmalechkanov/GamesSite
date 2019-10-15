9dd84fdff9b38bf5901052e69930e7b13ea5a9bb
Kaloyan Malechkanov <42181201+Kmalechkanov@users.noreply.github.com>
2019-10-15 20:00:38 +0300

    tempScript

---
+++ tempSrc/alien.js
@@ -0,0 +1,15 @@
+export class Alien {
+    constructor(x, y, width, height) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.health = null;
+    }
+
+    width;
+    height;
+    x;
+    y;
+    health;
+}
